6996f88ecb068ada56e1a5e540cdeef01477344a
Hugo Ruscitti <hugoruscitti@gmail.com>
2017-06-17 23:08:50 -0300

    Consultando variables de entornos.

--- app.py
+++ app.py
@@ -1,12 +1,19 @@
 import MySQLdb
-
+import os
 import flask
+
+DOKKU_APP_TYPE = os.environ['DOKKU_APP_TYPE']
 
 app = flask.Flask(__name__)
 
 @app.route('/')
 def temp():
-    return flask.jsonify(data={'demo': 123})
+    data = {
+        "demo": 123,
+        "DOKKU_APP_TYPE": DOKKU_APP_TYPE,
+    }
+
+    return flask.jsonify(data=data)
 
 if __name__ == '__main__':
     app.debug = True

d3faf3b57a3239143fada77408ffa82dfd609ece
Hugo Ruscitti <hugoruscitti@gmail.com>
2017-06-18 00:42:13 -0300

    Arreglo menor.

--- app.py
+++ app.py
@@ -2,16 +2,18 @@ import MySQLdb
 import os
 import flask
 from flask import request
+from urlparse import urlparse
 import re
 
-DATABASE_URL = os.environ['DATABASE_URL']
-DOKKU_APP_TYPE = os.environ['DOKKU_APP_TYPE']
+DATABASE_URL = os.environ.get('DATABASE_URL', None)
+DOKKU_APP_TYPE = os.environ.get('DOKKU_APP_TYPE', None)
 
 
 DB_USER = 'mysql'
 DB_PASSWORD = re.search(".*:(.*)@", DATABASE_URL).group(1)
-DB_HOST = re.search(".*@(.*:.*)\/.*", DATABASE_URL).group(1)
+DB_HOST = re.search(".*@(.*)\:.*", DATABASE_URL).group(1)
 DB_NAME = re.search(".*/(.*)", DATABASE_URL).group(1)
+
 
 app = flask.Flask(__name__)
 
@@ -22,25 +24,21 @@ def mysql_select_como_diccionario(query):
     cur.execute(query)
     return cur.fetchall()
 
-@app.route('/api/distritos')
-def distritos():
-    query = "SELECT * FROM s_distrito"
-    return convertir_en_respuesta('distritos', query)
-
-
 def convertir_en_respuesta(nombre_del_recurso, query):
     resultado = mysql_select_como_diccionario(query)
     cantidad = len(resultado)
     return flask.jsonify(cantidad=cantidad, nombre_del_recurso=resultado)
 
 
+@app.route('/api/distritos')
+def distritos():
+    query = "SELECT * FROM s_distrito"
+    return convertir_en_respuesta('distritos', query)
+
 @app.route('/')
 def index():
     ROOT_URL = request.host_url
     data = {
-        "demo": 123,
-        "ROOT_URL": ROOT_URL,
-        "DOKKU_APP_TYPE": DOKKU_APP_TYPE,
         "distritos": os.path.join(ROOT_URL, "api", "distritos")
     }
     return flask.jsonify(data=data)

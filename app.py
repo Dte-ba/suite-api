a8d70f6bed6b211edc259783734d2a9f556b5d8f
Hugo Ruscitti <hugoruscitti@gmail.com>
2017-06-17 23:55:35 -0300

    Configurando acceso a la base de datos.

--- app.py
+++ app.py
@@ -2,23 +2,27 @@ import MySQLdb
 import os
 import flask
 from flask import request
+import re
 
+DATABASE_URL = os.environ['DATABASE_URL']
 DOKKU_APP_TYPE = os.environ['DOKKU_APP_TYPE']
 
+
+DB_USER = 'mysql'
+DB_PASSWORD = re.search(".*:(.*)@", DATABASE_URL).group(1)
+DB_HOST = re.search(".*@(.*)\/.*", DATABASE_URL).group(1)
+DB_NAME = re.search(".*/(.*)", DATABASE_URL).group(1)
 
 app = flask.Flask(__name__)
 
 
 def mysql_select_como_diccionario(query):
-    """
-    db = MySQLdb.connect("localhost", "root", "","test")
+    db = MySQLdb.connect(DB_HOST, DB_USER, DB_PASSWORD, DB_NAME)
     cur = db.cursor()
-    cur.execute(query, param)
-    data = cur.fetchall()
+    cur.execute(query)
+    return cur.fetchall()
 
-    return data.json_encoder()
-    """
-    return None
+
 
 @app.route('/')
 def temp():
@@ -28,7 +32,6 @@ def temp():
         "ROOT_URL": ROOT_URL,
         "DOKKU_APP_TYPE": DOKKU_APP_TYPE,
     }
-
     return flask.jsonify(data=data)
 
 if __name__ == '__main__':
